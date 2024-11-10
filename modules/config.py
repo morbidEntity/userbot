f39aba6cbd0e045e4768c55215cbef1a48cb13e7
morbidEntity <marveliser77@gmail.com>
2024-11-10 16:02:04 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,5 @@
-from telethon.sessions import StringSession
 API_ID = 27056999
 API_HASH = '50f9e44c9ab978017476ba5237546d80'
-SESSION = StringSession()
+SESSION = ""
 ALIVE_IMAGE_PATH = '/storage/emulated/0/Download/alive_image.jpg'
 ABUSE_IMAGE_PATH = '/storage/emulated/0/Download/say.jpeg'
