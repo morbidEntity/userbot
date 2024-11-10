1724ec3d8a58babe7c86ec4ec8cbb5268404955c
morbidEntity <marveliser77@gmail.com>
2024-11-10 18:25:20 +0530

    Update abuse.py

--- modules/commands/abuse.py
+++ modules/commands/abuse.py
@@ -1,7 +1,7 @@
 from telethon import events
 from modules import config, logging
 
-ABUSE_IMAGE_URL='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaxicqMk6tjqJYHBaSWY0tEeMJBK_1O4Fra0Y4nNsfbA&s'
+ABUSE_IMAGE_URL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaxicqMk6tjqJYHBaSWY0tEeMJBK_1O4Fra0Y4nNsfbA&s'
 
 def setup(client):
     @client.on(events.NewMessage(pattern=r"\.abuse\s+(.+)"))
@@ -9,6 +9,6 @@ def setup(client):
         try:
             name = event.pattern_match.group(1)
             response = f"{name} teri maa ki chut!! 1,2,3,4 {name} ki gand maaro yar!"
-            await event.reply(response, file=ABUSE_IMAGE_URL)
+            await event.respond(response, file=ABUSE_IMAGE_URL)
         except Exception as e:
             logging.logger.error(f"Error in .abuse command: {e}")
