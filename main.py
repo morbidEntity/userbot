8009d511b01f85983bc213acfa296c9a5ae39eb9
morbidEntity <marveliser77@gmail.com>
2025-12-11 12:24:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telethon.sessions import StringSession
 from telethon import TelegramClient
 from modules import config, logging
-from modules.commands import alive, abuse, time, define, automate, flirt, quote, joke, fact
+from modules.commands import alive, abuse, time, define, automate, flirt, quote, joke, fact, afk
 import asyncio
 
 client = TelegramClient(StringSession(config.SESSION), config.API_ID, config.API_HASH)
@@ -16,6 +16,7 @@ flirt.setup(client)
 quote.setup(client)
 joke.setup(client)
 fact.setup(client)
+afk.setup(client)
 
 
 async def main():
